63161c1bbd26a0dbb01a4094e3de4e8b3c05bf28
euihyunee <lobil7782@gmail.com>
2025-04-28 19:17:38 +0900

    [fix] API fetch 부분 데이터 매핑 문제 수정

--- app/detail/page.jsx
+++ app/detail/page.jsx
@@ -5,10 +5,11 @@ import "./detailpage.css";
 import Image from "next/image";
 
 export default function DetailPage() {
-  const [movieData, setMovieData] = useState();
+  const [movieDetail, setMovieDetail] = useState();
+  const [actors, setActors] = useState([]);
 
   useEffect(() => {
-    const movieId = "22"; // 예시 movieId
+    const movieId = "1726"; // 예시 movieId
     const fetchMovie = async (movieId) => {
       const url = `http://localhost:8080/api/movie?movieId=${movieId}`;
       
@@ -20,7 +21,8 @@ export default function DetailPage() {
 
         const data = await response.json();
         console.log("영화 데이터:", data);
-        setMovieData(data);
+        setMovieDetail(data.movieDetailInfoDTO);
+        setActors(data.movieActorDTOList);
       } catch (error) {
         console.error("영화 정보를 불러오지 못했습니다:", error);
       }
@@ -29,7 +31,7 @@ export default function DetailPage() {
     fetchMovie(movieId);
   }, []);
 
-  if (!movieData) return <div>로딩 중...</div>;
+  if (!movieDetail) return <div>로딩 중...</div>;
 
   return (
     <>
@@ -43,10 +45,10 @@ export default function DetailPage() {
 
         {/* 영화 정보 영역 */}
         <div className="movie_details">
-          <h1 className="movie_title">{movieData.title}</h1>
+          <h1 className="movie_title">{movieDetail.title}</h1>
           <span id="movie_detail_info">
             <strong>개봉일:</strong> 
-            {movieData.releaseDate ? movieData.releaseDate : '정보 없음'}
+            {movieDetail.releaseDate ? movieDetail.releaseDate : '정보 없음'}
 
               {/* <strong>장르:</strong> {movieData.genres?.join(", ")} */}
               {/* <strong>러닝타임:</strong> {movieData.runTime}분 */}
@@ -54,7 +56,7 @@ export default function DetailPage() {
 
           <div className="movie_info">
             <div className="movie_like">❤</div>
-            <div className="movie_overview">{movieData.overview}</div>
+            <div className="movie_overview">{movieDetail.overview}</div>
           </div>
         </div>
       </div>
@@ -69,7 +71,7 @@ export default function DetailPage() {
         <div className="vote_stat_wrapper">
           <div className="vote_average">
             <h2>
-              {movieData.voteAverage} / {movieData.voteCount}명
+              {movieDetail.voteAverage} / {movieDetail.voteCount}명
             </h2>
           </div>
 
@@ -77,19 +79,19 @@ export default function DetailPage() {
           <div className="movie_stat">
             <div className="original_title">
               <h3>원래제목</h3>
-              <p>{movieData.originalTitle}</p>
+              <p>{movieDetail.originalTitle}</p>
             </div>
             <div className="status">
               <h3>상태</h3>
-              <p>{movieData.status}</p>
+              <p>{movieDetail.status}</p>
             </div>
             <div className="budget">
               <h3>제작비</h3>
-              <p>${movieData.budget}</p>
+              <p>${movieDetail.budget}</p>
             </div>
             <div className="revenue">
               <h3>수익</h3>
-              <p>${movieData.revenue}</p>
+              <p>${movieDetail.revenue}</p>
             </div>
           </div>
         </div>
@@ -98,7 +100,7 @@ export default function DetailPage() {
       {/* 배우 목록 */}
       <h2 className="actor_title">출연진</h2>
       <div className="actors_list">
-        {movieData.actors?.map((actor, index) => (
+        {movieDetail.actors?.map((actor, index) => (
           <div key={index} className="actor_item">
             <Image height={300} width={300}
               src={`http://image.tmdb.org/t/p/w185/${actor.profilePath}`}
